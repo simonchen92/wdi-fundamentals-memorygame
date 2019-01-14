c85b08b50a5c471699f068907d51c75decfd6c77
Simon Chen <simon@Simons-MacBook-Pro.local>
2019-01-14 14:22:45 -0500

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,23 +2,28 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-console.log(cardsInPlay.length === 2);
-
-console.log(cardsInPlay[0] === cardsInPlay[1]);
-
-
+var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-}
+};
+};
+
+
+var flipcard = function (cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch(2);
+};
+
+flipcard(0);
+flipcard(2);
+
+
+
+
+
+
+
 
