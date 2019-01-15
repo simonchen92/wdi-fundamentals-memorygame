04799c7aa408fb5459f4ccaef29203b1ca282b50
Simon Chen <simon@Simons-MacBook-Pro.local>
2019-01-15 14:47:36 -0500

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
 
 var cardsInPlay = [];
+
 
 var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -11,14 +33,18 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 };
 
 
-var flipcard = function (cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+var flipcard = function (cardId) {
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch(2);
 };
 
 flipcard(0);
 flipcard(2);
+
+
 
 
 
