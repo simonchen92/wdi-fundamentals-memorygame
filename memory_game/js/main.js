7d3980e8adad56bfbf7c5ea264dd0499df4f23bd
Simon Chen <simon@Simons-MacBook-Pro.local>
2019-01-16 16:21:54 -0500

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,27 +24,59 @@ var cards = [
 var cardsInPlay = [];
 
 
+// Alert message would pop up to tell us to see if cards match or not
+
 var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-};
+}
 };
 
 
-var flipcard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
+	
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
+	
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch(2);
+	
+// Once player select 2 cards - see if it match or not
+
+if (cardsInPlay.length === 2){
+	checkForMatch();
+};
 };
 
-flipcard(0);
-flipcard(2);
+console.log(cardsInPlay);
 
 
+var createBoard = function () {
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+}
+};
+
+
+// Reset Game
+var resetGame = function () {
+	for (var i = 0; i < cards.length; i++) {
+		document.getElementsByTagName("img")[i].setAttribute('src', 'images/back.png');
+};
+};
+
+
+
+createBoard();
+document.querySelector("button").addEventListener("click", resetGame);
 
 
 
